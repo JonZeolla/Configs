136abac5123a36f79918a507f397eb8fc8e312a2
Jon Zeolla <zeolla@gmail.com>
2018-02-15 12:11:45 -0500

    Add 7zip

--- windows/productivity/setup.ps1
+++ windows/productivity/setup.ps1
@@ -2,6 +2,6 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1))
 
 # Install things
-choco install git docker burp-suite-free-edition etcher nmap putty wireshark sublimetext3 winscp winpcap googlechrome packer virtualbox vagrant
+choco install git docker burp-suite-free-edition etcher nmap putty wireshark sublimetext3 winscp winpcap googlechrome packer virtualbox vagrant 7zip
 # Should upgrade this soon to 14
 choco install vmwareworkstation --version 12.5.9
