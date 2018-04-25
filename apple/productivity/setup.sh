280d12bbbde67ed2204a6a5ef235ea3210cc19a1
Jon Zeolla <zeolla@gmail.com>
2018-04-25 16:28:55 -0400

    Add snagit

--- apple/productivity/setup.sh
+++ apple/productivity/setup.sh
@@ -19,7 +19,7 @@ brew cask install java caskroom/versions/java8
 brew install python python3 go maven git wget gnupg2 ant npm yarn nmap bro swig cmake openssl jq azure-cli hashcat shellcheck packer bro nvm dos2unix testssl ttygif tree vim imagemagick ruby autoconf automake libtool
 npm install -g @angular/cli
 brew install fortune cowsay lolcat
-brew cask install vagrant virtualbox google-chrome sublime-text vmware-fusion rescuetime wireshark mysqlworkbench iterm2 slack steam firefox the-unarchiver gpgtools skype docker burp-suite etcher playonmac microsoft-teams atom powershell veracrypt beyond-compare drawio visual-studio-code little-snitch micro-snitch launchbar gfxcardstatus
+brew cask install vagrant virtualbox google-chrome sublime-text vmware-fusion rescuetime wireshark mysqlworkbench iterm2 slack steam firefox the-unarchiver gpgtools skype docker burp-suite etcher playonmac microsoft-teams atom powershell veracrypt beyond-compare drawio visual-studio-code little-snitch micro-snitch launchbar gfxcardstatus snagit
 brew install weechat --with-aspell --with-curl --with-python --with-perl --with-ruby --with-lua
 sudo easy_install pip
 # Twisted version is for sslstrip
