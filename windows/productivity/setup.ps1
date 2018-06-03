11ef905db2930992e0e07f474538b3938ea09585
Jon Zeolla <zeolla@gmail.com>
2018-06-02 22:41:37 -0400

    Typo

--- windows/productivity/setup.ps1
+++ windows/productivity/setup.ps1
@@ -1,5 +1,5 @@
 # Install chocolately
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1))
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install things
 choco install git docker burp-suite-free-edition etcher nmap putty wireshark sublimetext3 winscp winpcap googlechrome packer virtualbox vagrant 7zip
