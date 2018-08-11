5e47db30ba2b4a01ecb2807281540fc1d1f825a6
Jon Zeolla <zeolla@gmail.com>
2018-08-10 20:20:00 -0400

    Add some setup scripts for handy VMs

---
+++ ubuntu/productivity/setup_vm.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+## Always update first
+sudo apt update
+sudo apt -y upgrade
+
+# The following was selectively pulled from https://raw.githubusercontent.com/JonZeolla/Configs/master/apple/productivity/setup.sh on 2018-08-10
+
+## Install some basic tools
+sudo pip install virtualenv boto twisted==16.4.1 service_identity pyasn1-modules cryptography pyyaml
+sudo pip3 install boto3 paramiko selenium jupyter pyasn1-modules cryptography bcrypt asn1crypto ipaddress jedi docopt impacket pyyaml
+
+## Configure the environment
+wget -O ~/.bash_profile https://raw.githubusercontent.com/jonzeolla/configs/master/apple/productivity/.bash_profile
+wget -O ~/.bashrc https://raw.githubusercontent.com/jonzeolla/configs/master/apple/productivity/.bashrc
+wget -O ~/.bash_prompt https://raw.githubusercontent.com/jonzeolla/configs/master/apple/productivity/.bash_prompt
+source ~/.bash_profile
+wget -O ~/.screenrc https://raw.githubusercontent.com/jonzeolla/configs/master/apple/productivity/.screenrc
+mkdir -p ~/bin ~/etc ~/src/testing ~/src/seiso
+
+## Clone some repos
+cd ~/src
+git clone https://github.com/apache/metron
+git clone https://github.com/apache/metron-bro-plugin-kafka
+git clone https://github.com/bro/bro --recurse-submodules
+git clone https://github.com/jonzeolla/configs ~/src/jonzeolla/configs/
+git clone https://github.com/jonzeolla/development ~/src/jonzeolla/development/
+
+## Setup git
+wget -O ~/.gitconfig https://raw.githubusercontent.com/JonZeolla/Configs/master/apple/productivity/.gitconfig
+wget -O ~/src/seiso/.gitconfig https://raw.githubusercontent.com/JonZeolla/Configs/master/apple/productivity/.seisogitconfig
+
+## Setup GnuPG
+mkdir ~/.gnupg
+echo "use-standard-socket" >> ~/.gnupg/gpg-agent.conf
+
+## Setup vim
+# Setup pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# Pull down my .vimrc
+wget -O ~/.vimrc https://raw.githubusercontent.com/jonzeolla/configs/master/apple/productivity/.vimrc
+# Set up vim-sensible
+cd ~/.vim/bundle && git clone https://github.com/tpope/vim-sensible.git
+# Set up vim-colors-solarized
+cd ~/.vim/bundle && git clone git://github.com/altercation/vim-colors-solarized.git
+# Set up jedi-vim
+cd ~/.vim/bundle && git clone --recursive https://github.com/davidhalter/jedi-vim.git
+
