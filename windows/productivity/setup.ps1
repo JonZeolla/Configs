75f0d30b18b88dcd0797c2a73bc9f34d83d76747
Jon Zeolla <zeolla@gmail.com>
2019-01-08 21:22:14 -0500

    Add selenium/chrome driver

--- windows/productivity/setup.ps1
+++ windows/productivity/setup.ps1
@@ -2,6 +2,6 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install things
-choco install git docker burp-suite-free-edition etcher nmap putty wireshark sublimetext3 winscp winpcap googlechrome packer virtualbox vagrant 7zip vscode
+choco install git docker burp-suite-free-edition etcher nmap putty wireshark sublimetext3 winscp winpcap googlechrome packer virtualbox vagrant 7zip vscode node chromedriver
 # Should upgrade this soon to 14
 choco install vmwareworkstation --version 12.5.9
