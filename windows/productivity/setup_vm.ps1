07fc1b4a268c64d56f8307b5c911eeeeb316fe57
Jon Zeolla <zeolla@gmail.com>
2019-04-09 15:45:30 -0400

    Add choco cleaner and o365

--- windows/productivity/setup_vm.ps1
+++ windows/productivity/setup_vm.ps1
@@ -2,4 +2,4 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install things
-choco install git docker burp-suite-free-edition etcher nmap putty wireshark winscp winpcap googlechrome 7zip vscode
+choco install git docker burp-suite-free-edition etcher nmap putty wireshark winscp winpcap googlechrome 7zip vscode choco-cleaner office365business
