cb54e5d0fa757174f098f816f3cfbd2dcf0c7784
Jon Zeolla <zeolla@gmail.com>
2022-06-30 17:31:04 -0400

    Support docker and nerdctl in parallel

--- apple/productivity/bin/backtick.sh
+++ apple/productivity/bin/backtick.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env zsh
-num_running_containers=$(( $(nerdctl ps | wc -l) - 1  ))
-echo "${num_running_containers} containers running"
+num_docker_containers=$(( $(docker ps | wc -l) - 1  ))
+num_nerdctl_containers=$(( $(nerdctl ps | wc -l) - 1  ))
+total_running_containers=$(( ${num_docker_containers} + ${num_nerdctl_containers}  ))
+echo "${total_running_containers} containers running"
