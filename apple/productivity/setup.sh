f372ab1bcf5a631d0f508f2d996764c48ea364c0
Jon Zeolla <zeolla@gmail.com>
2024-06-28 19:28:19 -0400

    Install hatch

--- apple/productivity/setup.sh
+++ apple/productivity/setup.sh
@@ -36,7 +36,7 @@ eval "$(pyenv init -)"
 ###################################################################################
 
 # Packages useful to have on the host; project dependencies should be in a Pipfile.lock, requirements.txt, poetry.lock, etc.
-pip3 install bcrypt pylint termcolor flake8 defusedxml validators mypy black pytest-cov coverage virtualenv yamllint bandit scandir lxml grip cookiecutter pipx pre-commit gitpython pyyaml flynt refurb pyre gql aider-chat
+pip3 install bcrypt pylint termcolor flake8 defusedxml validators mypy black pytest-cov coverage virtualenv yamllint bandit scandir lxml grip cookiecutter pipx pre-commit gitpython pyyaml flynt refurb pyre gql aider-chat hatch
 brew install fd # for the linux-cultist/venv-selector.nvim plugin
 python3 -m pipx ensurepath
 pipx install pipenv
